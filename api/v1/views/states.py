31c72598456b36978febc9376ecde599d0a75db1
Emran <ouakouriimran@gmail.com>
2024-02-23 18:18:09 +0100

    pushito

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,87 +1,52 @@
 #!/usr/bin/python3
-"""States API routes"""
-from models import storage
-from flask import jsonify, request, abort
+"""a new view for State objects that handles all default RESTFul
+API actions"""
 from api.v1.views import app_views
 from models.state import State
+from models import storage
+from flask import jsonify, abort, request
 
 
-@app_views.route('/states/', methods=['GET'], strict_slashes=False)
-def show_states():
-    """Shows all states in storage
-           Returns:
-               A list of JSON dictionaries of all states in a
-               200 response body
-    """
-    states = list(storage.all('State').values())
-    states_list = []
-    for state in states:
-        states_list.append(state.to_dict())
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def all_states():
+    """Retrieves the list of all State objects"""
+    states = storage.all(State).values()
+    states_list = [s.to_dict() for s in states]
     return jsonify(states_list)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def show_state(state_id):
-    """Shows a specific state based on id from storage
-           Parameters:
-               state_id [str]: the id of the state to display
-
-           Returns:
-               A JSON dictionary of the state in a 200 response
-               A 404 response if the id does not match
-    """
-    state = storage.get('State', state_id)
-    if state:
-        return jsonify(state.to_dict())
-    else:
+def id_state(state_id):
+    """Retrieves a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
+    return jsonify(state.to_dict())
 
 
-@app_views.route(
-    '/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def delete_state(state_id):
-    """Deletes a specific state based on id from storage
-           Parameters:
-               state_id [str]: the id of the state to delete
-
-           Returns:
-               A JSON empty dictionary in a 200 response
-               A 404 response if the id does not match
-    """
-    state = storage.get('State', state_id)
-    if state:
-        storage.delete(state)
-        storage.save()
-        return jsonify({})
-    else:
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete(state_id):
+    """Deletes a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
 
 
-@app_views.route('/states/', methods=['POST'], strict_slashes=False)
-def create_state():
-    """Creates a state object
-           Returns:
-               A JSON dictionary of the new state in a 200 response
-               A 400 response if not a valid JSON or if missing parameters
-    """
-    content = request.get_json(silent=True)
-    error_message = ""
-    if type(content) is dict:
-        if "name" in content.keys():
-            state = State(**content)
-            storage.new(state)
-            storage.save()
-            response = jsonify(state.to_dict())
-            response.status_code = 201
-            return response
-        else:
-            error_message = "Missing name"
-    else:
-        error_message = "Not a JSON"
-
-    response = jsonify({'error': error_message})
-    response.status_code = 400
-    return response
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post():
+    """Creates a State"""
+    dict = request.get_json()
+    if dict is None:
+        abort(400, 'Not a JSON')
+    if dict.get("name") is None:
+        abort(400, 'Missing name')
+    new_status = State(**dict)
+    new_status.save()
+    return jsonify(new_status.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
