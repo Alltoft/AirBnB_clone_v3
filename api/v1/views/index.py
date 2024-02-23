327602247efda501e6d7ef9fe1174b90e8cb9d53
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-23 16:42:29 +0100

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,15 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def Json_status():
+    """return status route"""
     return (jsonify({"status": "OK"}))
 
-@app_views.route('/stats')
+
+@app_views.route('/stats', methods=['GET'])
 def number_obj():
+    """return numb of each obj"""
     return (jsonify({"amenity": storage.count("Amenity"),
                      "cities": storage.count("City"),
                      "places": storage.count("Place"),
