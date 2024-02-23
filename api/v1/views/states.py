cee3fa83a737c6225444ad1ec9ef97fd4378fe11
Emran <ouakouriimran@gmail.com>
2024-02-23 18:25:01 +0100

    pushito

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -57,45 +57,45 @@ def delete_state(state_id):
         abort(404)
 
 
-@app_views.route('/states/', methods=['POST'], strict_slashes=False)
-def create_state():
-    """Creates a state object
-           Returns:
-               A JSON dictionary of the new state in a 200 response
-               A 400 response if not a valid JSON or if missing parameters
-    """
-    content = request.get_json(silent=True)
-    error_message = ""
-    if type(content) is dict:
-        if "name" in content.keys():
-            state = State(**content)
-            storage.new(state)
-            storage.save()
-            response = jsonify(state.to_dict())
-            response.status_code = 201
-            return response
-        else:
-            error_message = "Missing name"
-    else:
-        error_message = "Not a JSON"
-
-    response = jsonify({'error': error_message})
-    response.status_code = 400
-    return response
-
-
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def put(state_id):
-    """Updates a State object"""
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post():
+    """Creates a State"""
     dict = request.get_json()
     if dict is None:
         abort(400, 'Not a JSON')
-    keys_substract = ['id', 'created_at', 'updated_at']
-    for key, val in dict.items():
-        if key not in keys_substract:
-            setattr(state, key, val)
-    storage.save()
-    return jsonify(state.to_dict()), 200
+    if dict.get("name") is None:
+        abort(400, 'Missing name')
+    new_status = State(**dict)
+    new_status.save()
+    return jsonify(new_status.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Updates an existing state object based on id
+           Parameters:
+               state_id [str]: the id of the state to update
+
+           Returns:
+               A JSON dictionary of the udpated state in a 200 response
+               A 400 response if not a valid JSON
+               A 404 response if the id does not match
+    """
+    state = storage.get('State', state_id)
+    error_message = ""
+    if state:
+        content = request.get_json(silent=True)
+        if type(content) is dict:
+            ignore = ['id', 'created_at', 'updated_at']
+            for name, value in content.items():
+                if name not in ignore:
+                    setattr(state, name, value)
+            storage.save()
+            return jsonify(state.to_dict())
+        else:
+            error_message = "Not a JSON"
+            response = jsonify({'error': error_message})
+            response.status_code = 400
+            return response
+
+    abort(404)
