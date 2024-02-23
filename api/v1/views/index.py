74075312b229f810bf4723e587e739d96912b574
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-23 16:44:07 +0100

    trying

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""Its time to start your API!"""
+""" index file """
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
-def Json_status():
-    """return status route"""
-    return (jsonify({"status": "OK"}))
+def status():
+    """ return status route """
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'])
-def number_obj():
-    """return numb of each obj"""
-    return (jsonify({"amenity": storage.count("Amenity"),
-                     "cities": storage.count("City"),
-                     "places": storage.count("Place"),
-                     "reviews": storage.count("Review"),
-                     "states": storage.count("State"),
-                     "users": storage.count("User")}))
+def stats():
+    """ return the number of each objects by type """
+    result = {}
+    obj_dict = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    for key, val in obj_dict.items():
+        result[val] = storage.count(key)
+    return jsonify(result)
