ce051c2a91c2b4824b7412d15c4f9d59ade9fa9c
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-23 17:02:40 +0100

    remove

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,7 +24,6 @@ def handle_404(exep):
 
 if __name__ == "__main__":
     """not importing module if it was imported"""
-
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
